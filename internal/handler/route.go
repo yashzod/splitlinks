3cefcbe83e8c004af9a90f483263f145e8b43b47
Yash Zod <yashzod748@gmail.com>
2025-08-04 19:53:34 +0530

    minor

--- internal/handler/route.go
+++ internal/handler/route.go
@@ -1,12 +1,18 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/yashzod/splitlinks/internal/service"
+
+	"github.com/yashzod/splitlinks/internal/repository"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB = repository.InitDB()
 
 func Route(w http.ResponseWriter, r *http.Request) {
 	slug := strings.TrimPrefix(r.URL.Path, "/r/")
@@ -26,4 +32,11 @@ func Route(w http.ResponseWriter, r *http.Request) {
 
 func CreateExperiment(w http.ResponseWriter, r *http.Request) {
 
+	var data map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Something went wrong.", http.StatusNotFound)
+	}
+	service.CreateExperiment(data, db)
+	fmt.Println(data)
 }
