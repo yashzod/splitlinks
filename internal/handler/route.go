2e628b7e7bb64a90e116bd920ed216a1db14954d
Yash Zod <55251183+yashzod@users.noreply.github.com>
2025-08-04 19:50:14 +0530

    Initialsetup (#4)
    * just startedgit add .
    
    * added redirection for random url
    
    * just startedgit add .
    
    * added redirection for random url
    
    * initial db and model setup

---
+++ internal/handler/route.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/yashzod/splitlinks/internal/service"
+)
+
+func Route(w http.ResponseWriter, r *http.Request) {
+	slug := strings.TrimPrefix(r.URL.Path, "/r/")
+	if slug == "" {
+		http.Error(w, "Missing slug", http.StatusBadRequest)
+		return
+	}
+
+	link, err := service.GetRedirectLink(slug)
+	if err != nil {
+		http.Error(w, "Url not found or expired", http.StatusNotFound)
+	}
+	fmt.Println("Redirecting slug:", link)
+
+	http.Redirect(w, r, link, http.StatusFound)
+}
+
+func CreateExperiment(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ internal/repository/sql.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"github.com/yashzod/splitlinks/internal/model" // update this import path as needed
+)
+
+func InitDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("yourapp.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect to SQLite database:", err)
+	}
+
+	// Auto-migrate your models
+	db.AutoMigrate(&model.Experiment{})
+	db.AutoMigrate(&model.Variant{})
+
+	return db
+}
